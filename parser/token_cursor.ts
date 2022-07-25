a95aeab989fbeadd853b4470c49d2fe85029b574
Tanvir Islam <tanvir.tinz@gmail.com>
2022-07-24 22:03:33 -0400

    more optimization

--- parser/token_cursor.ts
+++ parser/token_cursor.ts
@@ -4,49 +4,49 @@ const { EOF } = TokenType;
 
 // Layer to control "stream" of tokens.
 class TokenCursor {
-  private _index = -1;
-  private internalIndex = -1;
-  private tokens: Token[] = [];
-  private tokenizer: Tokenizer;
+  #index = -1;
+  #internalIndex = -1;
+  #tokens: Token[] = [];
+  #tokenizer: Tokenizer;
   public eofToken: Token | void = undefined;
 
   constructor(tokenizer: Tokenizer) {
-    this.tokenizer = tokenizer;
+    this.#tokenizer = tokenizer;
   }
 
-  private tokenize(moveCursor = true): TokenCursor {
+  #tokenize(moveCursor = true): TokenCursor {
     if (this.eofToken) {
-      if (moveCursor && this.index < this.tokens.length - 1) {
-        ++this.index;
+      if (moveCursor && this.index < this.#tokens.length - 1) {
+        ++this.#index;
       }
 
       return this;
     }
     // Function tokenize will keep returning the same
     // token called EOF if we hit the end of the line.
-    const token = this.tokenizer.tokenize();
+    const token = this.#tokenizer.tokenize();
 
     if (token.type === EOF) {
       this.eofToken = token;
     }
 
-    ++this.internalIndex;
+    ++this.#internalIndex;
 
     if (moveCursor) {
-      ++this.index;
+      ++this.#index;
     }
 
-    this.tokens.push(token);
+    this.#tokens.push(token);
 
     return this;
   }
 
   at(index: number): Token {
-    if (index > this.internalIndex) {
-      const times = index - this.internalIndex;
+    if (index > this.#internalIndex) {
+      const times = index - this.#internalIndex;
 
       for (let i = 0; i < times + 1; ++i) {
-        this.tokenize(false);
+        this.#tokenize(false);
 
         if (this.eofToken) {
           break;
@@ -55,19 +55,15 @@ class TokenCursor {
     }
 
     return this
-      .tokens[index > this.internalIndex ? this.internalIndex : index];
-  }
-
-  private set index(index: number) {
-    this._index = index;
+      .#tokens[index > this.#internalIndex ? this.#internalIndex : index];
   }
 
   get index() {
-    return this._index;
+    return this.#index;
   }
 
   get current(): Token {
-    return this.tokens[this.index];
+    return this.#tokens[this.index];
   }
 
   get next(): Token {
@@ -120,7 +116,7 @@ class TokenCursor {
   }
 
   advance(): TokenCursor {
-    return this.tokenize();
+    return this.#tokenize();
   }
 
   consume(query: string | TokenType): boolean {
