c98ccbf1fcbd76ec89463817d18868bd5d8eec92
Tanvir Islam <tanvir.tinz@gmail.com>
2022-07-26 19:38:47 -0400

    add watcher for debugging purposes

---
+++ scratchpad/watch.ts
@@ -0,0 +1,22 @@
+import { MinimizerVisitor, Parser } from "../parser/mod.ts";
+import { path } from "./deps.ts";
+
+const filename = path.join(
+  new URL(".", import.meta.url).pathname,
+  "scratchpad",
+);
+const watcher = Deno.watchFs(filename);
+
+for await (const _ of watcher) {
+  console.clear();
+  const parser = new Parser(Deno.readTextFileSync(filename));
+  try {
+    const ast = parser.parse();
+    const minimizerVisitor = new MinimizerVisitor();
+    const minimizedAst = minimizerVisitor.visit(ast);
+
+    console.log(JSON.stringify(minimizedAst, null, 2));
+  } catch (err) {
+    console.log(err.message);
+  }
+}
