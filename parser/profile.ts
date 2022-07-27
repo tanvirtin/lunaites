eb8f3dcf7a2420c59665b483d02804b8a5927dfa
Tanvir Islam <tanvir.tinz@gmail.com>
2022-07-27 01:05:57 -0400

    all green

--- parser/profile.ts
+++ parser/profile.ts
@@ -3,9 +3,9 @@ import { Parser } from "./mod.ts";
 
 const dirname = new URL(".", import.meta.url).pathname;
 const source = Deno.readTextFileSync(
-  `${dirname}/testdata/scratchpad/example_1`,
+  `${dirname}/testdata/bench/example_1`,
 );
-const epoch = 10e3;
+const epoch = 10e2;
 
 for (let i = 0; i < epoch; ++i) {
   const parser = new Parser(source);
